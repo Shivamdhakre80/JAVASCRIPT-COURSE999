5d9cb02022145527bb7375a45db3857351d62600
Shivamdhakre80 <shivamdhakre54@gmail.com>
2024-01-27 13:07:51 +0530

    Even numbers printing using while loop

---
+++ LOOPS/EvenNumbers.js
@@ -0,0 +1,12 @@
+// Even Numbers:
+// Write a program that prints all even numbers from 1 to 20 using a while loop.
+
+
+let i = 1;
+
+while (i <= 20) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+  i++;
+}
