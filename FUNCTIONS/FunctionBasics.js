6f2cf09c1c1f3dea46fc29d288512add6919c825
Shivamdhakre80 <shivamdhakre54@gmail.com>
2024-01-27 13:13:43 +0530

    Functions Basics

---
+++ FUNCTIONS/FunctionBasics.js
@@ -0,0 +1,14 @@
+// A function is a reusable block of code that performs a specific task. You can declare a function using the function keyword. Here's the basic syntax:
+
+/*
+function functionName(parameters) {
+    // code to be executed
+  }
+  
+  */
+
+
+  function addNumbers(a, b) {
+    let sum = a + b;
+    console.log("Sum:", sum);
+  }
